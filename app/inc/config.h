9160b24ce347143f5b3ccff86c934797cc7ac6e8
Yoon-Ki Hong <mymy49@nate.com>
2024-02-22 17:21:37 +0900

    [Crossworks for ARM에서 동작하는 코드 작성] - 단순히 타이머를 활용한 기능만 테스트하는 예제 포함

---
+++ app/inc/config.h
@@ -0,0 +1,75 @@
+/*
+	Copyright 2023. 홍윤기 all right reserved.
+
+	Permission is hereby granted, free of charge, to any person obtaining
+	a copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+	LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+	OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef YSS_CONFIG__H_
+#define YSS_CONFIG__H_
+
+// ####################### 외부 크리스탈 클럭 주파수 설정 #######################
+// 아래 HSE_CLOCK_FREQ가 정의 되어 있으면 HSE 클럭을 소스로 PLL 설정이 이뤄집니다.
+// 정의되어 있지 않으면 HSI 클럭을 소스로 PLL 설정이 이뤄집니다.
+// HSE 클럭은 4MHz의 배수를 갖는 주파수로 설정되어야 합니다.
+#define HSE_CLOCK_FREQ		24000000
+
+// ####################### 스케줄러 설정 #######################
+// 내부 ms 를 만들 시계의 타이머 설정 (timer1 ~ timer14)
+#define YSS_TIMER			timer6
+
+// 쓰레드당 할당 받는 Systick Clock의 수
+#define THREAD_GIVEN_CLOCK	20000
+
+// 최대 등록 가능한 쓰레드의 수
+#define MAX_THREAD			10
+
+// 쓰레드의 스택을 0xAA 패턴으로 채우기 (true, false)
+#define FILL_THREAD_STACK	false
+
+// ####################### GUI 설정 #######################
+// GUI library Enable (true, false)
+#define USE_GUI							false
+
+// ####################### KEY 설정 #######################
+// 최대 KEY 생성 가능 갯수 설정 (0 ~ ), 0일 경우 기능 꺼짐
+#define NUM_OF_YSS_KEY 0
+
+// ###################### 주변 장치 활성화 ######################
+// 활성화 시킬 장치에 대해 false -> true로 변경하여 활성화 한다.
+//
+// 주의 
+// 1. TIMER, PWM은 실제 동일한 장치지만 OS 구조상 별도의 장치로 표현한다. 그러므로 동일한 번호의 PWM과 TIMER는 동시에 활성화 되지 못한다.
+
+// TIMER 활성화
+#define TIM1_ENABLE			false
+#define TIM2_ENABLE			false
+#define TIM3_ENABLE			false
+#define TIM4_ENABLE			false
+#define TIM5_ENABLE			false
+#define TIM6_ENABLE			true
+#define TIM7_ENABLE			false
+#define TIM8_ENABLE			false
+#define TIM15_ENABLE		false
+#define TIM16_ENABLE		false
+#define TIM17_ENABLE		false
+
+#endif
+
+
